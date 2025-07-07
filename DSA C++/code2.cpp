b4ff1ee6a558ff97437b8ba6106e7e658828d97a
varun-2024 <v.talwar2024@gmail.com>
2025-07-07 10:56:38 +0100

    Enhance code2.cpp by adding precision output for float and double types, and include size outputs for these data types

--- DSA C++/code2.cpp
+++ DSA C++/code2.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main(){
     int a= 5, b=10;
     char d = 'E';
     bool isDone = true;
+    float pi = 3.14159265359;
+    double pi2 = 3.14159265359;
     cout<<a+b<<endl;
+    cout<<setprecision(12)<<pi<<endl;
+    cout<<setprecision(12)<<pi2<<endl;
     cout<<sizeof(int)<<endl;
     cout<<sizeof(char)<<endl;
     cout<<sizeof(bool)<<endl;
+    cout<<sizeof(float)<<endl;
+    cout<<sizeof(double)<<endl;
     return 0;
 }
