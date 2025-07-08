385f19fdf9bbf3fbad71cd80b5d5f58a9525c150
varun-2024 <v.talwar2024@gmail.com>
2025-07-08 10:25:19 +0100

    Stars 10x10 times

--- DSA C++/General Practice/code6.cpp
+++ DSA C++/General Practice/code6.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main(){
-    for (int i=0; i<4; i++){
-        for(int j=0; j<4; j++){
+    for (int i=0; i<10; i++){
+        for(int j=0; j<10; j++){
             cout<<"*";
         }
         cout<<endl;
