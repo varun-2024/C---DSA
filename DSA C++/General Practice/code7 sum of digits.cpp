ada660ec40c2e69e1928964a33b02b8b9941f97a
varun-2024 <v.talwar2024@gmail.com>
2025-07-08 11:53:19 +0100

    Sum of Digits of Number

--- DSA C++/General Practice/code7 sum of digits.cpp
+++ DSA C++/General Practice/code7 sum of digits.cpp
@@ -1,8 +1,12 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
-    int sum =0, num=10829;
+    int sum=0;
+    int num;
+    cout<<"Enter Number to calculate sum of Digits:"<<endl;
+    cin>>num;
+    
     while(num>0) {
         
         sum += num % 10;
