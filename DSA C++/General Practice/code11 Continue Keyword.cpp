483797873ed51e12002ca71703ee92d1e26e9a45
varun-2024 <v.talwar2024@gmail.com>
2025-07-09 10:49:32 +0100

    Continue keyword

--- DSA C++/General Practice/code11 Continue Keyword.cpp
+++ DSA C++/General Practice/code11 Continue Keyword.cpp
@@ -1,14 +1,21 @@
 #include<iostream>
 using namespace std;
-
 int main(){
     int i=0;
-    while(i<5){
+    while(i<=5){
         if(i==3){
+            i++;
             continue;
         }
         cout<<i<<endl;
         i++;
+    }
+    cout<<"Out of Loop 1 Now"<<endl;
+    for(i=0;i<=5; i++){
+        if(i==3){
+            continue;
+        }
+        cout<<i<<endl;
     }
     return 0;
 }
