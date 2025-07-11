be9fdf4b67b691b7caf64291090ec826b5bb0874
varun-2024 <v.talwar2024@gmail.com>
2025-07-11 18:41:26 +0100

    Fibonacci Series to Num

---
+++ DSA C++/General Practice/code24 Fibonacci Serries.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int num = 0;
+    cout << "Enter a number to print its Fibonacci Series up to that value:" << endl;
+    cin >> num;
+    if (num==0){
+        cout << "Fibonacci Serries for 0 is:"<<0<<endl;
+        return 0;
+    } else if (num == 1){
+        cout << "Fibonacci Serries for 1 is:"<< 0 <<", "<< 1 <<endl;
+        return 0;
+    }
+    int a= 0, b=1;
+    cout<< a << " , "<< b << " , ";
+    while(true){
+        int next = a + b;
+        if (next > num){
+            break;
+        }
+        cout << next << " , ";
+        a = b;
+        b = next;
+    }
+    return 0;
+}
