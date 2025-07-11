72d848dea042b17bffec29816d82dd00341e3fb7
varun-2024 <v.talwar2024@gmail.com>
2025-07-11 13:24:52 +0100

    1 to N  Prime Number Update Condition

--- DSA C++/General Practice/code23 Print 2 to N Prime Num.cpp
+++ DSA C++/General Practice/code23 Print 2 to N Prime Num.cpp
@@ -5,7 +5,11 @@ int main(){
     int num=0;
     cout<<"Enter a Number to Print Prime Number from 2 to Number:"<<endl;
     cin>>num;
-    for(int i=2; i<=num; i++){
+    if(num <2 ){
+        cout<<"Please Enter a Number Greater than 1"<<endl;
+        return 0;
+    } else{    
+        for(int i=2; i<=num; i++){
         bool isPrime = true;
         for(int j=2; j<=sqrt(i); j++){
             if( i % j == 0 ){
@@ -17,5 +21,6 @@ int main(){
             cout<<i<<endl;
         }
     }
+}
     return 0;
 }
