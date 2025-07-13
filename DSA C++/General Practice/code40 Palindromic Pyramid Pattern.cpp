968cb5b8d1b9f72084fbf02a3b55e3b9a369d337
varun-2024 <v.talwar2024@gmail.com>
2025-07-13 20:11:54 +0100

    Code for Palindromic Pyramid

--- DSA C++/General Practice/code40 Palindromic Pyramid Pattern.cpp
+++ DSA C++/General Practice/code40 Palindromic Pyramid Pattern.cpp
@@ -1,17 +1,24 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n =0;
-    cout<<"Enter no. of Rows:"<<endl;
+    int n=0;
+    cout << "Enter the No. of Rows:" << endl;
     cin>> n;
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=i; j++){
-            cout<< "   "; // Space
+    for(int i=1; i<=n; i++)
+    {
+        for( int j=1; j<=n-i; j++)
+        {
+            cout<< " ";
         }
-        for(int j=1; j<=n; j++){
-            cout<< " * "; // Star
+        for( int j=i; j>=1; j--)
+        {
+            cout<< j;
         }
-        cout << endl; // Last Star
+        for( int j=2; j<=i; j++)
+        {
+            cout<< j;
+        }
+        cout << endl;
     }
     return 0;
 }
