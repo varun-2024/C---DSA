fe2afc371303159ebbea49bd02591570f115ef82
varun-2024 <v.talwar2024@gmail.com>
2025-07-19 16:41:55 +0100

    Function Overloading

--- DSA C++/General Practice/code44 Functions.cpp
+++ DSA C++/General Practice/code44 Functions.cpp
@@ -6,12 +6,15 @@ void printHello(){
 }
 int addition(int a=3, int b=2);
 double addition(double a, double b);
+double addition(double a, double b, double c);
 
 int main(){
     printHello();
     cout<<addition(5)<<endl;
 
     cout<<addition(3.9, 2.4)<<endl;;
+
+    cout<<addition(3.9, 2.4, 1.2)<<endl;;
     return 0;
 }
 
@@ -20,4 +23,7 @@ int addition(int a, int b){
 }
 double addition(double a, double b){
     return a+b;
+}
+double addition(double a, double b, double c){
+    return a+b+c;
 }
