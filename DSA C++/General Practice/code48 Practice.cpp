e958391c0eff5c4f2c9c05032eee97b514f858fc
varun-2024 <v.talwar2024@gmail.com>
2025-07-21 11:30:22 +0100

    changes

--- DSA C++/General Practice/code48 Practice.cpp
+++ DSA C++/General Practice/code48 Practice.cpp
@@ -7,7 +7,7 @@ int main()
     char ch ='A'; // ASCII is 65
     char &cho = ch; // Stores Ch Refrence
     cho+= a; // cho = 65 + 32 ('A' + 32) = 97
-    *ptr+= ch; // *ptr/a = 32 + 97 = 129 ( a=32, ch becomes 97 in previous lin because ch and &cho are refrence for ch)
+    *ptr+= ch;  /* *ptr/a = 32 + 97 = 129 ( a=32, ch becomes 97 in previous line because ch and &cho are refrence for ch diffrent name for same variable) */
     cout<< a << " " << ch << endl;
     return 0;
 }
