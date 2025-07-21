c8b08467a9d1f91b6bb2faf2a21a072907890e7f
varun-2024 <v.talwar2024@gmail.com>
2025-07-21 15:38:09 +0100

    Pointers and Refrence Variables

--- DSA C++/General Practice/code46 Pointers.cpp
+++ DSA C++/General Practice/code46 Pointers.cpp
@@ -1,13 +1,25 @@
 #include<iostream>
 using namespace std;
+void changeA(int a){
+    a=25;
+    cout<<a<<endl;
+}
 void changeA(int *ptr)
 {
     *ptr = 10;
+    cout<<*ptr<<endl;
+}
+void changeB(int &ref){
+    ref = 50;
+    cout<< ref << endl;
 }
 int main()
 {
     int a=5;
-    changeA(&a);
-    cout<<a;
+    int &b = a;
+    changeA(a);
+    changeA(&b);
+    changeB(b);
+    cout<< a <<" "<< b <<endl;
     return 0;
 }
